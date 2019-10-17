d13af4d1131b5f3359948e13581494eb5027f1f5
carlromeo <carlromeo.dogbey@gmail.com>
2019-10-17 17:16:25 -0400

    first commit

---
+++ src/main/java/com/example/demo/WeekSevenChallengeSpringAtmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekSevenChallengeSpringAtmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekSevenChallengeSpringAtmApplication.class, args);
+    }
+
+}
